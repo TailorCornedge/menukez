b29bcbf23a16e05771d365b5374b6bcb4ef45bfc
TailorCornedge <szabok2@kkszki.hu>
2025-11-28 09:12:02 +0100

    menurendszer letrehozas

--- menukez/Program.cs
+++ menukez/Program.cs
@@ -10,7 +10,48 @@ namespace menukez
     {
         static void Main(string[] args)
         {
-            List<string> list = new List<string>();
+            Console.Clear();
+            Console.WriteLine("0 Kilépés");
+            Console.WriteLine("1 Néveleji szóközök levágva");
+            Console.WriteLine("2 Csak nagybetűvel kezdődők");
+            Console.WriteLine("3 Csak számjegy nélküliek");
+            Console.WriteLine("4 Csak speciális jel nélküliek");
+            Console.WriteLine("5 Legalább egy szóközt tartalmazók");
+            Console.WriteLine("6 Csak rendes nevek");
+            string valasz = Console.ReadLine();
+            if (valasz == "0")
+            {
+                return;
+            }
+            else if (valasz == "1")
+            {
+                //1
+            }
+            else if (valasz == "2")
+            {
+                return;
+            }
+            else if (valasz == "3")
+            {
+                return;
+            }
+            else if (valasz == "4")
+            {
+                return;
+            }
+            else if (valasz == "5")
+            {
+                return;
+            }
+            else if (valasz == "6")
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine("nincs ilyen parancs");
+            }
+                Console.ReadKey();
         }
     }
 }
