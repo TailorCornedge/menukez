dba045ccd2aef131051286c671c6f0bc907de92f
TailorCornedge <szabok2@kkszki.hu>
2025-11-28 09:15:11 +0100

    adatok készítése, feltöltése

--- menukez/Program.cs
+++ menukez/Program.cs
@@ -10,48 +10,57 @@ namespace menukez
     {
         static void Main(string[] args)
         {
-            Console.Clear();
-            Console.WriteLine("0 Kilépés");
-            Console.WriteLine("1 Néveleji szóközök levágva");
-            Console.WriteLine("2 Csak nagybetűvel kezdődők");
-            Console.WriteLine("3 Csak számjegy nélküliek");
-            Console.WriteLine("4 Csak speciális jel nélküliek");
-            Console.WriteLine("5 Legalább egy szóközt tartalmazók");
-            Console.WriteLine("6 Csak rendes nevek");
-            string valasz = Console.ReadLine();
-            if (valasz == "0")
+            List<string> nevek = new List<string>();
+            nevek.Add("Kiss István");
+            nevek.Add("Nagy elemér");
+            nevek.Add("kissándorjózsef23");
+            nevek.Add("Tóth# Ilona");
+            nevek.Add("Szabó Kornél");
+            while (true)
             {
-                return;
-            }
-            else if (valasz == "1")
-            {
-                //1
-            }
-            else if (valasz == "2")
-            {
-                return;
-            }
-            else if (valasz == "3")
-            {
-                return;
-            }
-            else if (valasz == "4")
-            {
-                return;
-            }
-            else if (valasz == "5")
-            {
-                return;
-            }
-            else if (valasz == "6")
-            {
-                return;
-            }
-            else
-            {
-                Console.WriteLine("nincs ilyen parancs");
-            }
+                Console.Clear();
+                Console.WriteLine("0 Kilépés");
+                Console.WriteLine("1 Néveleji szóközök levágva");
+                Console.WriteLine("2 Csak nagybetűvel kezdődők");
+                Console.WriteLine("3 Csak számjegy nélküliek");
+                Console.WriteLine("4 Csak speciális jel nélküliek");
+                Console.WriteLine("5 Legalább egy szóközt tartalmazók");
+                Console.WriteLine("6 Csak rendes nevek");
+                string valasz = Console.ReadLine();
+                if (valasz == "0")
+                {
+                    return;
+                }
+                else if (valasz == "1")
+                {
+                    //1
+                }
+                else if (valasz == "2")
+                {
+                    return;
+                }
+                else if (valasz == "3")
+                {
+                    return;
+                }
+                else if (valasz == "4")
+                {
+                    return;
+                }
+                else if (valasz == "5")
+                {
+                    return;
+                }
+                else if (valasz == "6")
+                {
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("nincs ilyen parancs");
+                }
                 Console.ReadKey();
+            }
         }
     }
 }
